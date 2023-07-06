6672f9a8d6d682535fd62c185a2e8091ffb437f7
chsi1995 <chsi1995@kakao.com>
2023-07-06 06:53:39 +0000

    로고 안나오는 문제

--- src/App.js
+++ src/App.js
@@ -1,43 +1,52 @@
 import { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
 
 const staticServerUri = process.env.REACT_APP_PATH || '';
-console.log(staticServerUri);
 
 function App() {
-    useEffect(() => {
-        axios
-            .get('https://api.github.com/users/EdenKim-dev')
-            .then((res) => {
-                console.log(res);
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-    }, []);
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route
-                    path={staticServerUri + '/'}
-                    element={
-                        <div>
-                            <header>
-                                <img src alt="logo" />
-                                <p>
-                                    Edit <code>src/App.js</code> and save to reload.
-                                </p>
-                                <a href="https://reactjs.org" target="_blank">
-                                    Learn React
-                                </a>
-                            </header>
-                        </div>
-                    }
-                ></Route>
-            </Routes>
-        </BrowserRouter>
-    );
+	useEffect(() => {
+		console.log(staticServerUri);
+
+		axios
+			.get('https://api.github.com/users/EdenKim-dev')
+			.then((res) => {
+				console.log(res);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}, []);
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path={staticServerUri + '/'}
+					element={
+						<div className="App">
+							<header className="App-header">
+								<img src={logo} className="App-logo" alt="logo" />
+								<p>
+									Edit <code>src/App.js</code> and save to reload.
+								</p>
+								<a
+									className="App-link"
+									href="https://reactjs.org"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									Learn React
+								</a>
+							</header>
+						</div>
+					}
+				></Route>
+				<Route path={staticServerUri + '/login'} element={<div>로그인 페이지</div>} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
