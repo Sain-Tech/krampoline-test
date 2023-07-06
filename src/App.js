8507ebfc7e78ea81abcff80f82f15b53bdef7344
chsi1995 <chsi1995@kakao.com>
2023-07-06 03:47:24 +0000

    init commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+function App() {
+    useEffect(() => {
+        axios
+            .get('https://api.github.com/users/EdenKim-dev')
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }, []);
+    return (
+        <div>
+            <header>
+                <img src alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a href="https://reactjs.org" target="_blank">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
